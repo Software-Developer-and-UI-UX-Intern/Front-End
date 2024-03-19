94562dc5a62cd4eae95bc0527813751c03e8d987
SpexID <ngurahadipermana15@gmail.com>
2024-03-19 13:04:18 +0800

    feat: recomended hotel button

--- src/components/beranda/ayojalan.tsx
+++ src/components/beranda/ayojalan.tsx
@@ -1,7 +1,7 @@
 
 import Box from '@mui/material/Box';
 import Wisata from '../../assets/beranda/GWK.jpg'
-import { Stack } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 
 // import { Container } from '@mui/material';
 
@@ -9,14 +9,21 @@ import { Stack } from '@mui/material';
 export default function RowAndColumnSpacing() {
   return (
     <Box height='385px'  sx={{borderRadius:'40px'}}>
-      <Stack width='auto' sx={{ borderRadius:'40px', background: `url(${Wisata}) center center / cover no-repeat`,
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover"}}>
+      <Stack width='100%' sx={{ borderRadius:'40px', background: `url(${Wisata}) center center / cover no-repeat`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover'}}>
       
      <img src={Wisata} width='100%' style={{borderRadius:'40px 0 40px 0'}} >
      
      </img>
-     
+     <Stack marginTop={'-80px'} justifyContent={'center'} alignItems={'center'} paddingBottom={'20px'}>
+     <Typography color={'white'} sx={{fontSize:'32px', fontWeight:500}}>
+      Garuda Wisnu Kencana
+     </Typography>
+     <Typography color={'white'} sx={{fontSize:'28px', fontWeight:500}}>
+      Bali
+     </Typography>
+     </Stack>
      </Stack>
     </Box>
   );
--- src/pages/Beranda/Beranda.tsx
+++ src/pages/Beranda/Beranda.tsx
@@ -1,46 +1,81 @@
-import {
-  
-  Button,
-  Stack, Typography,
-  
-} from '@mui/material';
-import { useEffect } from 'react'; // Import useEffect hook
-import '@fontsource/poppins/500.css';
-import '@fontsource/poppins/700.css';
-// import '@fontsource/Telkomsel Batik Sans.css'
-import bg  from '../../assets/beranda/main.jpg';
-import {BerandaButton} from '../../components/beranda/rekomenhotel';
+// Beranda.tsx
+import React, { useEffect, useState } from 'react';
+import { Stack, Typography, Button, Grid } from '@mui/material';
+import bg from '../../assets/beranda/main.jpg';
+import { RekomenHotel } from '../../components/beranda/rekomenhotel';
+import RowAndColumnSpacing from '../../../src/components/beranda/ayojalan';
+import {Buttonslider}  from '../../../src/components/beranda/buttonslider';
 import logoTripsel from '../../assets/Trip-sel.png';
 import logoTelkomsel from '../../assets/Telkomsel (full).png';
-import RowAndColumnSpacing from '../../../src/components/beranda/ayojalan';
-import Grid from '@mui/material/Grid';
 
+interface Hotel {
+  id: number;
+  name: string;
+  stars: string;
+  image: string;
+}
 
+interface HotelRecommendationsProps {
+  hotels: Hotel[];
+}
 
+const HotelRecommendations: React.FC<HotelRecommendationsProps> = ({ hotels }) => {
+  return (
+    <Stack direction={'row'} spacing={2}>
+      {hotels.map((hotel) => (
+        <RekomenHotel
+          key={hotel.id}
+          name={hotel.name}
+          stars={hotel.stars}
+          image={hotel.image}
+        />
+      ))}
+    </Stack>
+  );
+};
 
 export default function Beranda() {
+  const [hotelData, setHotelData] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
+
   useEffect(() => {
-    document.body.style.margin = '0'; // Set body margin to 0
-    document.body.style.padding = '0'; // Optionally set body padding to 0 as well
+    fetch('https://tripselbe.fly.dev/recommendation')
+      .then(response => response.json())
+      .then(data => setHotelData(data))
+      .catch(error => console.error('Error fetching hotel data:', error));
+  }, []);
+
+  useEffect(() => {
+    document.body.style.margin = '0';
+    document.body.style.padding = '0';
     document.body.style.marginTop = '-120px';
   }, []);
+
+  const itemsPerPage = 2;
+  const totalPages = Math.ceil(hotelData.length / itemsPerPage);
+
+  const startIndex = activeIndex * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
   return (
     <Stack sx={{
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
-      marginLeft:'0px',}} gap={0}>
+      marginLeft: '0px',
+    }} gap={0}>
 
       <Stack sx={{
         background: `url(${bg}) center center / cover no-repeat`,
         backgroundSize: 'cover',
         display: 'flex',
-        height: '1100px', 
+        height: '1100px',
         justifyContent: 'center',
-        width: '100%', 
-        margin: '0',    
+        width: '100%',
+        margin: '0',
       }}>
       </Stack>
+
       <Stack sx={{
         display: 'flex',
         height: '1024px',
@@ -49,23 +84,23 @@ export default function Beranda() {
         backgroundColor: '#04214c',
         borderRadius: '0 0 100px 100px',
       }}>
-      
-        <Typography sx={
-                {
-                fontWeight: 500,
-                color: 'white',
-                fontSize: '60px',
-                paddingTop: '50px',
-                textAlign: 'center'}}>
+
+        <Typography sx={{
+          fontWeight: 500,
+          color: 'white',
+          fontSize: '60px',
+          paddingTop: '50px',
+          textAlign: 'center'
+        }}>
           Bali-Nusra Menunggumu
         </Typography>
-        <Typography sx={
-        {
+
+        <Typography sx={{
           fontWeight: 400,
           color: 'white',
           fontSize: '25px',
-          textAlign: 'center'}}>
-
+          textAlign: 'center'
+        }}>
           Jelajahi seluruh regionalnya sekarang!
         </Typography>
       </Stack>
@@ -75,112 +110,94 @@ export default function Beranda() {
         height: '2030px',
         width: '100%',
         paddingTop: '50px',
-        // paddingRight: '100px',
-        backgroundColor: 'white',}}>
-      
+        backgroundColor: 'white',
+      }}>
+
         <Stack sx={{
-        height: '1000px',
-        width: '100%',
-        backgroundColor: 'white',}}>
-            
-            <Stack sx={{
-              height: '250px',
-              width: '100%',
-              backgroundColor: 'white',
-              borderBottomRightRadius: '500px',
-              }}>
-              <Typography sx={{
-                marginLeft: '100px',
-                fontWeight: 700,
-                color: '#ff010c',
-                fontSize: '60px',
-                alignItems: 'center',
-                }}>
-                Rekomendasi Hotel
-              </Typography>
+          height: '1000px',
+          width: '100%',
+          backgroundColor: 'white',
+        }}>
 
-              <Typography sx={{
-                marginLeft: '100px',
-                fontWeight: 400,
-                color: '#04214c',
-                fontSize: '30px',
-                alignItems: 'center'}}>
-                Khusus T-Flyers aja lho~
-              </Typography>
-              
-            </Stack>
-
-            <Stack sx={{
-              display: 'flex',
-              height: 'auto',
-              width: 'auto',
+          <Stack sx={{
+            height: '250px',
+            width: '100%',
+            backgroundColor: 'white',
+            borderBottomRightRadius: '500px',
+          }}>
+            <Typography sx={{
               marginLeft: '100px',
-              marginRight: '100px',
-              marginBottom: '50px',
-              backgroundColor: 'white'}} direction='row' gap={5}>
-              <BerandaButton/>
-              <BerandaButton/>
-              <BerandaButton/>
-            </Stack>
+              fontWeight: 700,
+              color: '#ff010c',
+              fontSize: '60px',
+              alignItems: 'center',
+            }}>
+              Rekomendasi Hotel
+            </Typography>
 
-              <Stack gap={5} direction='row' justifyContent='center' alignContent='center'>
-              <Button sx={{alignContent: 'center', width: '100px', height: '40px', borderRadius: '30px', backgroundColor: '#ff010c'}}>   
-              </Button>
+            <Typography sx={{
+              marginLeft: '100px',
+              fontWeight: 400,
+              color: '#04214c',
+              fontSize: '30px',
+              alignItems: 'center'
+            }}>
+              Khusus T-Flyers aja lho~
+            </Typography>
 
-              <Button sx={{alignContent: 'center', width: '40px', height: '40px', borderRadius: '30px', backgroundColor: '#ff010c'}}>
-              </Button>
+          </Stack>
 
-                <Button sx={{alignContent: 'center', width: '40px', height: '40px', borderRadius: '30px', backgroundColor: '#ff010c'}}>
-                </Button>
-                
-              </Stack>
-              <Stack justifyContent='center' alignContent='center' alignItems='center' sx={{marginTop: '35px'}}>
-                <Button sx={{alignContent: 'center', width: 'auto', height: 'auto', borderRadius: '30px', backgroundColor: '#ff010c'}}>
-                  <Typography sx={{
-                    padding: '5px 25px', 
-                    color: 'white', 
-                    fontWeight:'600',
-                    '&:hover': { width: 'auto', height: 'auto', background: 'white', color: 'red', boxShadow: '0px 0px 0px 2px red', borderRadius: '30px' },}}>
-                      Jelajahi
-                      </Typography>
-                </Button>
-
-              </Stack>
-
-              <Typography sx={
-                {
-                fontWeight: 700,
-                color: '#ff010c',
-                fontSize: '60px',
-                paddingTop: '50px',
-                textAlign: 'center'}}>
-                
-                Ayo Jalan-Jalan
+          <Stack sx={{
+            display: 'flex',
+            height: 'auto',
+            width: 'auto',
+            marginLeft: '100px',
+            marginRight: '100px',
+            marginBottom: '50px',
+            backgroundColor: 'white'
+          }} direction='row' gap={5}>
+            <HotelRecommendations hotels={hotelData.slice(startIndex, endIndex)} />
+          </Stack>
+          {hotelData.length > 0 && <Buttonslider activeIndex={activeIndex} setActiveIndex={setActiveIndex} totalPages={totalPages} />}
+          <Stack justifyContent='center' alignContent='center' alignItems='center' sx={{ marginTop: '35px' }}>
+            <Button sx={{ alignContent: 'center', width: '288px', height: '80px', borderRadius: '40px', backgroundColor: '#ff010c' }}>
+              <Typography sx={{
+                color: 'white',
+                fontWeight: '600',
+                fontSize:'32px',
+                '&:hover': { width: 'auto', height: 'auto', background: 'white', color: 'red', boxShadow: '0px 0px 0px 2px red', borderRadius: '30px' }
+              }}>
+                Jelajahi
               </Typography>
+            </Button>
+          </Stack>
+
+          <Typography sx={{
+            fontWeight: 700,
+            color: '#ff010c',
+            fontSize: '60px',
+            paddingTop: '50px',
+            textAlign: 'center'
+          }}>
+            Ayo Jalan-Jalan
+          </Typography>
         </Stack>
-        
-        <Grid container rowSpacing={7} columnSpacing={5} alignItems='center' justifyContent='center'  borderRadius='40px'>
-        <Grid item lg={5}  borderRadius='40px'>
-          
-            <RowAndColumnSpacing/>
-            
+
+        <Grid container rowSpacing={7} columnSpacing={5} alignItems='center' justifyContent='center' borderRadius='40px'>
+          <Grid item lg={5} borderRadius='40px'>
+            <RowAndColumnSpacing />
+          </Grid>
+          <Grid item lg={5}>
+            <RowAndColumnSpacing />
+          </Grid>
+          <Grid item lg={5}>
+            <RowAndColumnSpacing />
+          </Grid>
+          <Grid item lg={5}>
+            <RowAndColumnSpacing
+            />
+          </Grid>
         </Grid>
-        <Grid item lg={5}>
-          
-          <RowAndColumnSpacing/>
-          
-        </Grid>
-        <Grid item lg={5}>
-          
-          <RowAndColumnSpacing/>
-          
-        </Grid>
-        <Grid item lg={5}>
-          
-          <RowAndColumnSpacing/>
-          
-        </Grid>
-      </Grid>
 
       </Stack>
 
@@ -190,149 +207,121 @@ export default function Beranda() {
         width: '100%',
         margin: '0',
         backgroundColor: '#04214c',
-        borderRadius: '100px 100px 0 0',}}>
-          <Typography sx={{
-                marginLeft: '100px',
-                marginTop: '50px',
-                fontWeight: 500,
-                color: 'white',
-                fontSize: '55px',
-                alignItems: 'center',
-                textAlign: 'center'}}>
-                Pengalaman Trip-Sel
-              </Typography>
-
-
+        borderRadius: '100px 100px 0 0',
+      }}>
+        <Typography sx={{
+          marginLeft: '100px',
+          marginTop: '50px',
+          fontWeight: 500,
+          color: 'white',
+          fontSize: '55px',
+          alignItems: 'center',
+          textAlign: 'center',
+        }}>
+          Pengalaman Trip-Sel
+        </Typography>
       </Stack>
 
-        
-        <Stack direction='row' sx={{
+      <Stack direction='row' sx={{
         display: 'flex',
         height: '440px',
         width: '100%',
         margin: '0',
         backgroundColor: 'white',
+      }}>
+        <Stack width='200px' sx={{
+          padding: '175px 100px',
+          alignContent: 'center',
         }}>
-            <Stack width='200px' sx={
-              {
-                padding: '175px 100px',
-                alignContent: 'center',
+          <img src={logoTripsel} alt="Logo Tripsel" />
+          <Stack direction='row' gap={1} paddingTop={1}>
+            <Typography>
+              by
+            </Typography>
+            <img src={logoTelkomsel} alt="Logo Telkomsel" width='100px' />
+          </Stack>
+        </Stack>
 
-              }
-            }>
-            <img src={logoTripsel} alt="Logo Tripsel" />
-            <Stack direction = 'row' gap={1} paddingTop={1}>
-              <Typography>
-                 by
-              </Typography>
-            <img src={logoTelkomsel} alt="Logo Telkomsel" width='100px'/>
-            </Stack>
+        <Stack sx={{
+          padding: '50px',
+        }}>
+          <Typography sx={{
+            color: '#04214c',
+            fontWeight: 600,
+            fontSize: 25,
+          }}>
+            Tentang Trip-sel
+          </Typography>
 
-            </Stack>
+          <Typography sx={{
+            color: '#04214c',
+            fontWeight: 400,
+            fontSize: 20,
+            paddingTop: 1,
+          }}>
+            Tentang Kami
+          </Typography>
 
-            <Stack sx={
-              {
-                padding: '50px',
-              }
-            }>
-              <Typography sx={
-                {
-                  color: '#04214c',
-                  fontWeight: 600,
-                  fontSize: 25,
-                  
-                }
-              }>
-                Tentang Trip-sel
-              </Typography>
+          <Typography sx={{
+            color: '#04214c',
+            fontWeight: 400,
+            fontSize: 20,
+            paddingTop: 1,
+          }}>
+            Kontak
+          </Typography>
 
-              <Typography sx={
-                {
-                  color: '#04214c',
-                  fontWeight: 400,
-                  fontSize: 20,
-                  paddingTop: 1,
-                }
-              }>
-                Tentang Kami
-              </Typography>
+          <Typography sx={{
+            color: '#04214c',
+            fontWeight: 600,
+            fontSize: 25,
+            paddingTop: 7.5,
+          }}>
+            Ikuti Perjalanan Trip-sel
+          </Typography>
 
-              <Typography sx={
-                {
-                  color: '#04214c',
-                  fontWeight: 400,
-                  fontSize: 20,
-                  paddingTop: 1,
-                }
-              }>
-                Kontak
-              </Typography>
+          <Typography sx={{
+            color: '#04214c',
+            fontWeight: 400,
+            fontSize: 20,
+            paddingTop: 1,
+          }}>
+            Instagram
+          </Typography>
 
-              <Typography sx={
-                {
-                  color: '#04214c',
-                  fontWeight: 600,
-                  fontSize: 25,
-                  paddingTop: 7.5,
-                }
-              }>
-                
-                Ikuti Perjalanan Trip-sel
-              </Typography>
+          <Typography sx={{
+            color: '#04214c',
+            fontWeight: 400,
+            fontSize: 20,
+            paddingTop: 1,
+          }}>
+            TikTok
+          </Typography>
 
-              <Typography sx={
-                {
-                  color: '#04214c',
-                  fontWeight: 400,
-                  fontSize: 20,
-                  paddingTop: 1,
-                }
-              }>
-                Instagram
-              </Typography>
-
-              <Typography sx={
-                {
-                  color: '#04214c',
-                  fontWeight: 400,
-                  fontSize: 20,
-                  paddingTop: 1,
-                }
-              }>
-                TikTok
-              </Typography>
-
-              <Typography sx={
-                {
-                  color: '#04214c',
-                  fontWeight: 400,
-                  fontSize: 20,
-                  paddingTop: 1,
-                }
-              }>
-                YouTube
-              </Typography>
-
-            </Stack>
-
-            <Typography sx={
-                {
-                  color: '#04214c',
-                  fontWeight: 600,
-                  fontSize: 25,
-                  paddingTop: 7.5,
-                  paddingLeft: 15,
-                }
-              }>
-                
-                Ada kendala dengan Trip-mu?<br></br>
-                QOS-an hadir disini!
-              </Typography>
+          <Typography sx={{
+            color: '#04214c',
+            fontWeight: 400,
+            fontSize: 20,
+            paddingTop: 1,
+          }}>
+            YouTube
+          </Typography>
 
         </Stack>
 
-    </Stack>
+        <Typography sx={{
+          color: '#04214c',
+          fontWeight: 600,
+          fontSize: 25,
+          paddingTop: 7.5,
+          paddingLeft: 15,
+        }}>
+          Ada kendala dengan Trip-mu?<br></br>
+          QOS-an hadir disini!
+        </Typography>
 
-    
+      </Stack>
+
+    </Stack>
   );
 }
