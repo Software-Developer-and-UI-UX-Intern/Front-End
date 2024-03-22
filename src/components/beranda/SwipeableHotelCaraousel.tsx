eb3e9345c12ff4991ca3d2015b27476cc4e8ce6c
SpexID <ngurahadipermana15@gmail.com>
2024-03-22 15:28:36 +0800

    fix: swipeableviews fix attempt 2

--- src/components/beranda/SwipeableHotelCaraousel.tsx
+++ src/components/beranda/SwipeableHotelCaraousel.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { useTheme } from '@mui/material/styles';
-// @ts-expect-error depedencies eror fix, react swipeable not compatible with react 18
 import SwipeableViews from 'react-swipeable-views-react-18-fix';
 import { RekomenHotel } from '../beranda/rekomenhotel'; // Assuming this is the correct path to your RekomenHotel component
 import { Buttonslider } from './buttonslider'; // Assuming this is the correct path to your Buttonslider component
